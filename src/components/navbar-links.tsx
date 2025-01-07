addc5c4d8aa15a30deaa7c366e31b8c9a066f049
allan <allanrey.macasaquit@gmail.com>
2025-01-07 10:31:51 -0500

    3. Update navlinks screen

--- src/components/navbar-links.tsx
+++ src/components/navbar-links.tsx
@@ -4,6 +4,12 @@ import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { Category } from '@/app/types';
+import { Button } from '@/components/ui/button';
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover';
 
 interface CategoryProps {
 	data: Category[];
@@ -20,20 +26,51 @@ const NavbarLinks = ({ data }: CategoryProps) => {
 	}));
 
 	return (
-		<nav className='flex space-x-4 text-gray-300'>
-			{routes.map((route) => (
-				<Link
-					key={route.href}
-					href={route.href}
-					className={cn(
-						'px-3 py-2 rounded-md hover:bg-gray-700 hover:text-white',
-						route.active && 'bg-blue-600 text-white'
-					)}
-				>
-					{route.label}
-				</Link>
-			))}
-		</nav>
+		<div className='relative'>
+			{/* Mobile Navigation (Popover for Small Screens) */}
+			<Popover>
+				<PopoverTrigger asChild>
+					<Button
+						variant='ghost'
+						className='md:hidden text-gray-300 hover:bg-gray-900 hover:text-white'
+					>
+						Menu
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent className='w-screen border-none  p-2 mt-10 bg-gray-900/95'>
+					<nav className='flex flex-col space-y-2'>
+						{routes.map((route) => (
+							<Link
+								key={route.href}
+								href={route.href}
+								className={cn(
+									'block px-4 py-2 rounded-md hover:bg-gray-900 text-gray-500 hover:text-gray-300',
+									route.active && 'bg-blue-600 text-gray-300'
+								)}
+							>
+								{route.label}
+							</Link>
+						))}
+					</nav>
+				</PopoverContent>
+			</Popover>
+
+			{/* Desktop Navigation */}
+			<nav className='hidden md:flex space-x-4 text-gray-300'>
+				{routes.map((route) => (
+					<Link
+						key={route.href}
+						href={route.href}
+						className={cn(
+							'px-3 py-2 rounded-md hover:bg-gray-700 hover:text-white',
+							route.active && 'bg-blue-600 text-white'
+						)}
+					>
+						{route.label}
+					</Link>
+				))}
+			</nav>
+		</div>
 	);
 };
 
